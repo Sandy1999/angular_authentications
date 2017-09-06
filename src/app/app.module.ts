53a931624efb5da6407e84f8df5696108cc90c7a
Sanidhya Mangal <sandyrockstar19@gmail.com>
2017-09-06 22:54:53 +0530

    first commit with full sturture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { NotAccessComponent } from './not-access/not-access.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    AdminComponent,
+    NotAccessComponent,
+    NotFoundComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule
